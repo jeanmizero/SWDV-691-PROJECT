4d14926d8ba06ae4418091f7245b85c31e1d25b1
Jean Mizero <mizero2@gmail.com>
2022-04-02 17:39:42 -0600

    Add admin and models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
+# Add Categories Models
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
+    
+    class Meta:
+        # sort by name
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    
+    def get_url(self):
+      return reverse('products_by_category', args=[self.slug])
+      
+    # Display output
+    def __str__(self):
+        return self.name
+    
+# Create Product Class Model
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    # Category relationship
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        # sort by name
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+    
+    # Display output
+    def __str__(self):
+        return self.name
+      
+    
 
 # Create your models here.
--- store/views.py
+++ store/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category,Product
 
 
 # Homepage
-def home(request):
-  return render(request, 'home.html')
+def home(request, category_slug=None):
+  category_page = None
+  products = None
+  # Display Products
+  if category_slug != None:
+    category_page = get_object_or_404(Category, slug = category_slug)
+    products = Product.objects.filter(category = category_page, available = True)
+  else:
+    products = Product.objects.all().filter(available = True) 
+  return render(request, 'home.html', {'category': category_page, 'products': products})
 # About page
 def productPage(request):
   return render(request, 'product.html')
